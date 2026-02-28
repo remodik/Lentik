829a3a6173efb00e572deb365a56898d25fe14ea
slend <slenderzet@gmail.com>
2026-02-28 20:41:24 +0300

    feat: обновление конфигурации Next.js для использования переменной окружения API_URL и добавление маршрутов для статических файлов

--- services/web/next.config.mjs
+++ services/web/next.config.mjs
@@ -1,12 +1,19 @@
+/** @type {import('next').NextConfig} */
+const API_URL = process.env.API_URL ?? "http://localhost:8000";
+
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:8000/:path*",
-            },
-        ];
-    },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/:path*`,
+      },
+      {
+        source: "/static/:path*",
+        destination: `${API_URL}/static/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
